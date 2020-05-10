9c2d40080972698823e1e060f7e003507f77c559
unknown <giremio5@gmail.com>
2020-05-10 18:07:18 -0300

    updates

--- src/app/cadaster/cadaster.page.ts
+++ src/app/cadaster/cadaster.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {HelperService} from '../services/helper.service';
+import { HttpClient } from 'selenium-webdriver/http';
+import {NgForm} from '@angular/forms';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-cadaster',
@@ -6,10 +10,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadaster.page.scss'],
 })
 export class CadasterPage implements OnInit {
+ 
+  constructor(
+    private helper: HelperService,
+    //private authentication: AuthenticationService,
+    private http: HttpClient) { }
 
-  constructor() { }
+  email = this.getEmail();
+  contactField: null;
+  messageField: null;
 
   ngOnInit() {
   }
 
+  getEmail() {
+    const email = this.helper.getUrlParameter('email');
+    console.log(email);
+    return email;
+  }
+
+  sendForm(f: NgForm) {
+  //   const url = environment.apiUrl + '/saveForm';
+  //   const dataIn = {
+  //     contact: this.contactField,
+  //     message: this.messageField,
+  //   //  token: this.authentication.token
+  //   };
+  //   this.http.post(url, dataIn)
+  //     .subscribe(dataOut => {
+  //       console.log(dataOut);
+  //     }, error => {
+  //       console.log(error.message);
+  //     });
+   }
 }
